bb285cbd694f057cb76b991cf6b7ef1f54aceeb7
Alexandru2401 <a.toma231@yahoo.com>
2024-07-26 15:06:19 +0300

    First push

---
+++ src/Products.jsx
@@ -0,0 +1,23 @@
+import { data } from "./api/api";
+
+export default function Products() {
+  return (
+    <div>
+      {data.map((data) => {
+        return (
+          <div key={data.id} style={{ width: "600px" }}>
+            <h2>{data.name}</h2>
+            <img src={data.image} style={{ height: "300px" }} />
+            <p>{data.description}</p>
+            <ul>
+              {data.recipeIngredient &&
+                data.recipeIngredient.map((ingredient) => (
+                  <li>{ingredient}</li>
+                ))}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
