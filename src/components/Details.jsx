fd0556e2183a7309c41d534c3e6baf317ff5810d
Alexandru2401 <a.toma231@yahoo.com>
2024-07-29 17:28:46 +0300

    structuring and styling the details page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -3,6 +3,7 @@ import { Card, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { data } from "../api/api";
 
+import Accordion from "react-bootstrap/Accordion";
 export default function Details() {
   let { id } = useParams();
   const foundProduct = data.find((product) => product._id === id);
@@ -12,12 +13,21 @@ export default function Details() {
   }
 
   return (
-    <Container>
-      <Card key={id}>
-        <Card.Title>{foundProduct.name}</Card.Title>
-        <Card.Img variant="top" src={foundProduct.image} />
-        <Card.Body>
+    <Container className="my-4" >
+      <Card key={id} style={{backgroundColor:"#ECB176"}}> 
+        <Card.Img variant="top" src={foundProduct.image} style={{maxWidth:"800px"}} className="mx-auto my-3"/>
+        <Card.Body style={{maxWidth:"800px"}} className="mx-auto">
+          <Card.Title>{foundProduct.name}</Card.Title>
           <Card.Text>{foundProduct.description}</Card.Text>
+          <Card.Text style={{fontWeight:"bold"}}>Ingredients:</Card.Text>
+          <Accordion defaultActiveKey="0">
+            {foundProduct.recipeIngredient.map((ingredient, index) => (
+              <Accordion.Item eventKey={String(index)} key={index}>
+                <Accordion.Header>Ingredient {index + 1}</Accordion.Header>
+                <Accordion.Body>{ingredient}</Accordion.Body>
+              </Accordion.Item>
+            ))}
+          </Accordion>
         </Card.Body>
       </Card>
     </Container>
