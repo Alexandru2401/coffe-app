7eec4fe3595071cd0d55fc1c8bb9fde622ac2907
Alexandru2401 <a.toma231@yahoo.com>
2024-07-29 13:22:36 +0300

    updape genralInfo style

--- src/components/HomeComponents/GeneralInfo.jsx
+++ src/components/HomeComponents/GeneralInfo.jsx
@@ -1,82 +1,114 @@
-import { Container, Card, Row, Col } from "react-bootstrap";
-
+import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 export default function GeneralInfo() {
   // This part will contain some info about products
   return (
     <Container className="my-5 d-flex flex-column align-items-center">
       <Row>
-        <h2>General Info</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe
-          nemo ad perferendis ipsam et cum est, soluta deleniti tempore!
-          Laboriosam, dolorem debitis dolore iste nobis unde quidem error minus?
-        </p>
+        <Col>
+          <h2>General Info</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde saepe
+            nemo ad perferendis ipsam et cum est, soluta deleniti tempore!
+            Laboriosam, dolorem debitis dolore iste nobis unde quidem error
+            minus?
+          </p>
+        </Col>
       </Row>
 
-      <Row>
-        <Col>
+      <Row className="w-100 d-flex justify-content-center">
+        <Col xs={12} md={6} lg={3}>
           <Card style={{ width: "18rem" }}>
             <Card.Body>
-              <Card.Title></Card.Title>
+              <Card.Title>No chemical</Card.Title>
               <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
+                Organic Coffee
               </Card.Subtitle>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                maiores a sapiente deleniti nihil, dolore nemo molestiae nostrum
+                illo nam similique.
               </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title></Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
-              </Card.Subtitle>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <div className="d-flex justify-content-between">
+                <Link to="/products">
+                  <Button>See products</Button>
+                </Link>
+                <Link to="/ingredients">
+                  <Button>See ingredients</Button>
+                </Link>
+              </div>
             </Card.Body>
           </Card>
         </Col>
 
-        <Col>
+        <Col xs={12} md={6} lg={3}>
           <Card style={{ width: "18rem" }}>
             <Card.Body>
-              <Card.Title></Card.Title>
+              <Card.Title>No Preservatives</Card.Title>
               <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
+                Fresh products
               </Card.Subtitle>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                maiores a sapiente deleniti nihil, dolore nemo molestiae nostrum
+                illo nam similique.
               </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <div className="d-flex justify-content-between">
+                <Link to="/products">
+                  <Button>See products</Button>
+                </Link>
+                <Link to="/ingredients">
+                  <Button>See ingredients</Button>
+                </Link>
+              </div>
             </Card.Body>
           </Card>
         </Col>
 
-        <Col>
+        <Col xs={12} md={6} lg={3}>
           <Card style={{ width: "18rem" }}>
             <Card.Body>
-              <Card.Title></Card.Title>
+              <Card.Title>No Additives</Card.Title>
               <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
+                No allergens
               </Card.Subtitle>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                maiores a sapiente deleniti nihil, dolore nemo molestiae nostrum
+                illo nam similique.
               </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <div className="d-flex justify-content-between">
+                <Link to="/products">
+                  <Button>See products</Button>
+                </Link>
+                <Link to="/ingredients">
+                  <Button>See ingredients</Button>
+                </Link>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col xs={12} md={6} lg={3}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Just Coffee</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Real Coffee
+              </Card.Subtitle>
+              <Card.Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                maiores a sapiente deleniti nihil, dolore nemo molestiae nostrum
+                illo nam similique.
+              </Card.Text>
+              <div className="d-flex justify-content-between">
+                <Link to="/products">
+                  <Button>See products</Button>
+                </Link>
+                <Link to="/ingredients">
+                  <Button>See ingredients</Button>
+                </Link>
+              </div>
             </Card.Body>
           </Card>
         </Col>
