de9ef75daa87e526eff40c7d3a83c85bf991143d
Alexandru2401 <a.toma231@yahoo.com>
2024-09-09 17:15:29 +0300

    adding store for cart state management

--- src/pages/PayPage.jsx
+++ src/pages/PayPage.jsx
@@ -1,7 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import PayForm from "../components/payComponents/PayForm";
-import Cart from "../comp
-onents/payComponents/Cart";
+import Cart from "../components/payComponents/Cart";
 export default function PayPage() {
   return (
     <Container>
